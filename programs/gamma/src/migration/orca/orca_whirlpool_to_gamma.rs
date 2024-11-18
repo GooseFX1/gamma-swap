009c4b13ead893477e591fc865b4901115c9facc
0xabstracted <104343984+0xabstracted@users.noreply.github.com>
2024-11-18 22:07:48 +0530

    calculate token_vault balances along with fees

--- programs/gamma/src/migration/orca/orca_whirlpool_to_gamma.rs
+++ programs/gamma/src/migration/orca/orca_whirlpool_to_gamma.rs
@@ -177,12 +177,16 @@ pub fn orca_whirlpool_to_gamma<'info>(
     let token_0_amount_withdrawn = user_token0_balance_before.checked_sub(user_token0_balance_after).unwrap();
     let token_1_amount_withdrawn = user_token1_balance_before.checked_sub(user_token1_balance_after).unwrap();
     let pool_state = ctx.accounts.gamma_pool_state.load()?;
+    let (token_0_vault_amount_without_fee, token_1_vault_amount_without_fee) = pool_state.vault_amount_without_fee(
+        ctx.accounts.gamma_token_0_vault.amount, 
+        ctx.accounts.gamma_token_1_vault.amount,
+    )?;
     let gamma_lp_tokens = calculate_gamma_lp_tokens(
         token_0_amount_withdrawn, 
         token_1_amount_withdrawn, 
         &pool_state,
-        ctx.accounts.gamma_token_0_vault.amount,
-        ctx.accounts.gamma_token_1_vault.amount,
+        token_0_vault_amount_without_fee,
+        token_1_vault_amount_without_fee,
     )?;
 
     let mut deposit_accounts = Deposit {
--- programs/gamma/src/migration/raydium/raydium_clmm_to_gamma_v2.rs
+++ programs/gamma/src/migration/raydium/raydium_clmm_to_gamma_v2.rs
@@ -189,12 +189,16 @@ pub fn raydium_clmm_to_gamma_v2<'a, 'b, 'c, 'info>(
     let token_0_amount_withdrawn = user_token0_balance_before.checked_sub(user_token0_balance_after).unwrap();
     let token_1_amount_withdrawn = user_token1_balance_before.checked_sub(user_token1_balance_after).unwrap();
     let pool_state = ctx.accounts.gamma_pool_state.load()?;
+    let (token_0_vault_amount_without_fee, token_1_vault_amount_without_fee) = pool_state.vault_amount_without_fee(
+        ctx.accounts.gamma_token_0_vault.amount, 
+        ctx.accounts.gamma_token_1_vault.amount,
+    )?;
     let gamma_lp_tokens = calculate_gamma_lp_tokens(
         token_0_amount_withdrawn, 
         token_1_amount_withdrawn, 
         &pool_state,
-        ctx.accounts.gamma_token_0_vault.amount,
-        ctx.accounts.gamma_token_1_vault.amount,
+        token_0_vault_amount_without_fee,
+        token_1_vault_amount_without_fee,
     )?;
 
     let mut deposit_accounts = Deposit {
