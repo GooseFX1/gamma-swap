40128fdeecf1e31a0535e04f1edb62efb6f655ef
Sushant Chandla <sushantchandla@gmail.com>
2025-05-12 14:19:39 +0530

    fix: oracle based swap amount calculation for normal swap

--- programs/gamma/tests/utils/jupiter2.rs
+++ programs/gamma/tests/utils/jupiter2.rs
@@ -593,11 +593,11 @@ impl OracleBasedSwapCalculator {
             .ok_or(anyhow!("Math overflow"))?;
 
         let new_swap_source_amount = swap_source_amount
-            .checked_sub(amount_to_be_swapped_at_oracle_price)
+            .checked_add(amount_to_be_swapped_at_oracle_price)
             .ok_or(anyhow!("Math overflow"))?;
 
         let new_swap_destination_amount = swap_destination_amount
-            .checked_add(output_tokens)
+            .checked_sub(output_tokens)
             .ok_or(anyhow!("Math overflow"))?;
 
         let trade_fees_for_invariant_curve = ceil_div(
